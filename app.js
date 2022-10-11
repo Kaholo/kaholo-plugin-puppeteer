1795187762d935ab246b28c526a3ed07b69aae7e
xkcm <xkcm16@gmail.com>
2022-10-11 22:06:28 +0200

    Implement autocomplete function for "JavaScript Puppeteer Test" parameter

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const { bootstrap } = require("@kaholo/plugin-library");
 
 const puppeteerService = require("./puppeteer-service");
+const autocomplete = require("./autocomplete");
 
 module.exports = bootstrap({
   runPuppeteerTest: puppeteerService.runPuppeteerTest,
-});
+}, autocomplete);
